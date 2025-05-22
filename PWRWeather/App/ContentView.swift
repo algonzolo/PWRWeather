b4350fb5d4a506ff15efcdf1aa07dde4fda9e86a
Albert Garipov <garipov102@gmail.com>
2025-05-22 17:26:22 +0300

    Initial Commit

---
+++ PWRWeather/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PWRWeather
+//
+//  Created by Albert Garipov on 22.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PWRWeather/App/PWRWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PWRWeatherApp.swift
+//  PWRWeather
+//
+//  Created by Albert Garipov on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PWRWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
